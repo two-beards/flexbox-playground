11394f631536335758a8ea009da9a6e3d0439e8c
Mike McBride <mmcbride1007@gmail.com>
2020-02-20 11:46:25 -0600

    add github icon

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,7 +4,15 @@
       Flexbox Playground
       <span class="ml-2 py-1 pl-2 border-l border-gray-400 font-normal text-sm text-gray-700">&nbsp;A web app for exploring Flexbox</span>
     </span>
-    <span>
+    <span class="items-center flex">
+      <a
+        href="https://github.com/mikemcbride/flexbox-playground/"
+        target="_blank"
+        rel="noreferrer noopener"
+        title="View on GitHub"
+        class="no-underline text-gray-600 hover:text-gray-900 inline-block mr-4 w-6 h-6">
+        <GitHubIcon />
+      </a>
       <button
         class="text-sm cursor-pointer rounded px-4 py-2 border-none mr-2 inline-block text-indigo-600 bg-gray-200 hover:bg-gray-300 font-semibold"
         @click="resetDefaults">
@@ -22,8 +30,13 @@
 
 <script>
 import { mapActions } from 'vuex'
+import GitHubIcon from '@/components/GitHubIcon'
+
 export default {
   name: 'AppHeader',
+  components: {
+    GitHubIcon,
+  },
   methods: {
     ...mapActions(['toggleMarkup', 'resetDefaults'])
   }
